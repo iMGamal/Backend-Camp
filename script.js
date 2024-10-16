4197125400701d4f4a029ad2ca1ff34c5fbf3559
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-16 01:45:19 +0000

    JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("JavaScript is working!")
+console.log("JavaScript is working!");
+
+document.getElementById('about').innerHTML = '<p>This is my updated bio!</p>';
+
+document.getElementById('contact').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	let name = document.getElementById('name').value;
+	let email = document.getElementById('email').value;
+	let message = document.getElementById('message').value;
+
+	if (name && email) {
+		message.textContent = "Form submitted successfully!";
+		message.style.color = "red";
+	}
+});

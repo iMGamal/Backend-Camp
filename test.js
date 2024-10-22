710aea918bfe14a556dd770ebe668dde0cbbc9a3
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-22 20:37:19 +0000

    Demo

---
+++ test.js
@@ -0,0 +1,12 @@
+function variables() {
+
+	let second = "Hello";
+	console.log(second);
+
+	const third = "there!";
+	console.log(third);
+
+	var first = console.log("Mohamed,");
+}
+
+variables();
